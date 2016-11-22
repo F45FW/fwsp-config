8d6be57821a8e6ade3726dd2fdad11f74761b95a
emadum <eric@flywheelsports.com>
2016-11-22 13:37:13 -0500

    added options to reconnect redis and logger on config update

--- hydra.js
+++ hydra.js
@@ -1,11 +1,24 @@
 'use strict';
 
 const UMFMessage = require('fwsp-umf-message');
+const Logger = require('fwsp-logger');
 const EventEmitter = require('events');
 
+/**
+ * @name HydraConfig
+ * @summary Hydra config helper
+ * @fires HydraConfig#HydraConfig.CONFIG_UPDATE_EVENT
+ */
 class HydraConfig extends EventEmitter {
-  constructor(hydra) {
+  /**
+   * @param {object} hydra - hydra instance
+   * @param {object} options - options
+   */
+  constructor(hydra, options={}) {
     super();
+    this.hydraOptions = Object.assign({
+      reconnectRedis: false
+    }, options);
     this.hydra = hydra;
     hydra.on('message', msg => this.handleMessage(msg));
     let serviceName = hydra.getServiceName();
@@ -14,34 +27,80 @@ class HydraConfig extends EventEmitter {
       from: `${serviceName}`,
       body: {}
     })
-      .then(result => {
-        if (result && result && Object.keys(result).length) {
-          this.emit(HydraConfig.CONFIG_UPDATE_EVENT, result);
+      .then(response => {
+        if (response && response.statusCode === 200 && Object.keys(response.result).length) {
+          /**
+           * @event HydraConfig#HydraConfig.CONFIG_UPDATE_EVENT
+           * @type {object}
+           */
+          this.emit(HydraConfig.CONFIG_UPDATE_EVENT, response.result);
         }
       });
   }
+
+  /**
+   * @return {string} config update event
+   * @static
+   */
   static get CONFIG_UPDATE_EVENT() {
     return 'configUpdate';
   }
+
+  /**
+   * @return {string} update message type
+   * @static
+   */
   static get UPDATE_MESSAGE_TYPE() {
     return 'configRefresh';
   }
-  setHydraExpress(hydraExpress) {
+
+  /**
+   * @name setHydraExpress
+   * @param {object} hydraExpress - hydra express instance
+   * @param {object} options - options
+   */
+  setHydraExpress(hydraExpress, options={}) {
+    this.hydraExpressOptions = Object.assign({
+      reconnectLogger: false
+    }, options);
     this.on(HydraConfig.CONFIG_UPDATE_EVENT, config => {
-      let error = hydraExpress.validateConfig(config);
-      if (!error) {
-        hydraExpress.config = config;
-      } else {
-        console.log('Error validating config update', error);
+      // pull version and registerRoutesCallback, set at service initialization, from existing config
+      config.version = hydraExpress.config.version;
+      config.registerRoutesCallback = hydraExpress.config.registerRoutesCallback;
+      let missing = hydraExpress.validateConfig(config);
+      if (missing.length) {
+        console.log('Missing required fields: ' + missing.join(', '));
+        return;
+      }
+      hydraExpress.config = config;
+      if (this.hydraExpressOptions.reconnectLogger && config.logger) {
+        const logger = new Logger({
+                name: config.logger.name || config.hydra.serviceName
+              },
+              config.logger.elasticsearch
+            );
+        let oldLogger = hydraExpress.logger;
+        hydraExpress.logger = logger;
+        hydraExpress.appLogger = logger.getLogger();
+        console.log('new logger attached');
+        if (oldLogger) {
+          oldLogger.shutdown(code => {
+            console.log('old logger shut down');
+          });
+        }
       }
     });
   }
+
+  /**
+   * @name handleMessage
+   * @param {object} msg - message object
+   */
   handleMessage(msg) {
     let message = UMFMessage.createMessage(msg);
     if (message.from === 'config-service:/') {
       if (message.type !== HydraConfig.UPDATE_MESSAGE_TYPE) {
         console.log(`Unrecognized message type: ${message.type}`);
-        console.dir(message, {colors:true});
         return;
       }
       if (message.body.targetVersion) {
@@ -57,6 +116,21 @@ class HydraConfig extends EventEmitter {
         console.log('Need to target a version or *');
       }
       this.hydra.config = message.body.config.hydra;
+      if (this.hydraOptions.reconnectRedis) {
+        this.hydra.redisdb.once('end', () => {
+          console.log('[config] Redis connection closed.');
+          this.hydra._connectToRedis(this.hydra.config);
+        });
+        this.hydra.redisdb.once('ready', () => {
+          console.log('[config] Redis reconnected');
+        });
+        console.log('[config] Requesting redis shutdown...');
+        this.hydra.redisdb.quit();
+      }
+      /**
+       * @event HydraConfig#HydraConfig.CONFIG_UPDATE_EVENT
+       * @type {object}
+       */
       this.emit(HydraConfig.CONFIG_UPDATE_EVENT, message.body.config);
     }
   }
