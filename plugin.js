f8f44002a9b1d4096ed7d836b778c502f505221e
emadum <eric@flywheelsports.com>
2016-12-14 12:23:12 -0500

    implemented config plugins

---
+++ plugin.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const ConfigPlugin = require('./lib/ConfigPlugin');
+const ExpressConfigPlugin = require('./lib/ExpressConfigPlugin');
+
+module.exports = {ConfigPlugin, ExpressConfigPlugin};
